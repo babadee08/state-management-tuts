994049cfa50b673cd144a483f3a822fba423ba6c
Damilare Durojaye <damilare.durojaye@gmail.com>
2019-08-11 01:44:15 +0100

    Created movie state and rendered it in a component using props

---
+++ src/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Nav() {
+    return (
+        <div>
+            <header>
+                <h4>Babadee08</h4>
+                <h6>Number of movies = 0</h6>
+            </header>
+        </div>
+    )
+}
+
+export default Nav;
